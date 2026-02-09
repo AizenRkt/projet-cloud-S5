57a8605393b4cdff8256f34b48a92b8e453a8c6d
Fenitra12 <fenitra00@gmail.com>
2026-02-09 19:52:06 +0300

    vita

--- bd/script.sql
+++ bd/script.sql
@@ -141,9 +141,11 @@ INSERT INTO type_signalement (nom, icon) VALUES
 
 -- Insertion des types de statut
 INSERT INTO signalement_type_status (code, libelle, pourcentage) VALUES
+    ('en_attente', 'En attente', 0),
     ('nouveau', 'Nouveau', 0),
     ('en_cours', 'En cours', 50),
-    ('termine', 'Terminé', 100);
+    ('termine', 'Terminé', 100),
+    ('annule', 'Annulé', 0);
 
 -- Insertion de signalements de test à Antananarivo
 INSERT INTO signalement (id_type_signalement, latitude, longitude, description, surface_m2, budget, id_entreprise) VALUES
@@ -155,6 +157,10 @@ INSERT INTO signalement (id_type_signalement, latitude, longitude, description, 
 
 -- Insertion des statuts initiaux
 INSERT INTO signalement_status (id_signalement, id_signalement_type_status) VALUES
-    (1, 1), (2, 1), (3, 1), (4, 1), (5, 1);
+    (1, (SELECT id_signalement_type_status FROM signalement_type_status WHERE code = 'en_cours')),
+    (2, (SELECT id_signalement_type_status FROM signalement_type_status WHERE code = 'en_cours')),
+    (3, (SELECT id_signalement_type_status FROM signalement_type_status WHERE code = 'en_cours')),
+    (4, (SELECT id_signalement_type_status FROM signalement_type_status WHERE code = 'en_cours')),
+    (5, (SELECT id_signalement_type_status FROM signalement_type_status WHERE code = 'en_cours'));
 
 
